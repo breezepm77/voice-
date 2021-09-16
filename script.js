9bf8b1dffac0229c63f7fe20cafeb306c9a21802
rofiqusmonov613 <rofiqusmonov613@gmail.com>
2021-09-16 17:11:32 +0500

    voice

---
+++ script.js
@@ -0,0 +1,50 @@
+const texts = document.querySelector('.texts');
+
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const recognition = new window.SpeechRecognition();
+recognition.interimResults = true;
+
+let p = document.createElement('p');
+
+recognition.addEventListener('result', (e) => {
+
+    const text = Array.from(e.results)
+    .map(result => result[0])
+    .map(result => result.transcript)
+    .join('');
+
+    p.innerText = text;
+    texts.appendChild(p);
+
+    if(e.results[0].isFinal){
+        if(text.includes('hello')){
+            p = document.createElement('p');
+            p.classList.add('replay');
+            p.innerText = 'Hi';
+            texts.appendChild(p)
+        }
+            if(text.includes('what is your name') || text.includes("what's your name")) {
+                p = document.createElement('p');
+                p.classList.add('replay');
+                p.innerText = "My name's Rofiq, Yours?";
+                texts.appendChild(p)
+            }
+            if(text.includes("Open new GitHub repository")) {
+                p = document.createElement('p');
+                p.classList.add('replay');
+                p.innerText = "Opening new github repository";
+                texts.appendChild(p)
+                window.open('https://github.com/new')
+            }
+        p = document.createElement('p');
+    }
+
+    console.log(e);
+})
+
+recognition.addEventListener('end', ()=>{
+    recognition.start();
+})
+
+recognition.start();
