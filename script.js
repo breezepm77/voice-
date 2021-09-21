4ecfdc9287884b98ca3d3e4c26c91304a022cebb
rofiqusmonov <87475791+rofiqusmonov@users.noreply.github.com>
2021-09-21 10:38:06 +0500

    Update script.js

--- script.js
+++ script.js
@@ -18,14 +18,13 @@ recognition.addEventListener('result', (e) => {
 
     p.innerText = text;
     texts.appendChild(p);
-    console.log(text)
     if(e.results[0].isFinal){
-            if(text.includes()) {
+            if(text.includes("")) {
                 p = document.createElement('p');
                 p.classList.add('replay');
                 p.innerText = "Opening kun.uz";
                 texts.appendChild(p)
-                window.open(`https://www.google.com/search?q=${texts.results}`)
+                window.open(`https://www.google.com/search?q=${text.results}`)
             }else if(text.includes("учебный центр")){
                 p = document.createElement('p');
                 p.classList.add('replay');
